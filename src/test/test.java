098bb03ef705877a0859bdb00f22d402fcb94bc2
Reishandy <isthisruxury@gmail.com>
2023-03-28 18:48:56 +0700

    initial

---
+++ src/test/test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        for (int i = 0; i < 100; i++) {
+            System.out.printf("i is %d", i);
+        }
+    }
+}
